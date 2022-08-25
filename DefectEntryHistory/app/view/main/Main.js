b1aaad7ac275c202f116e89c4a795823a4049885
Christian-Iglesias <christian_iglesias@goodyear.com>
2022-08-25 15:20:07 +0800

    Initial Layout of MainView
    Added Toolbar

--- DefectEntryHistory/app/view/main/Main.js
+++ DefectEntryHistory/app/view/main/Main.js
@@ -1,5 +1,30 @@
 Ext.define('DefectEntryHistory.view.main.Main', {
   extend: 'Ext.panel.Panel',
   xtype: 'app-main',
+  layout: 'border',
   title: 'Defect Entry History Rpt',
+  dockedItems: [
+    {
+      dock: 'top',
+      items: [
+        '->',
+        {
+          xtype: 'button',
+          iconCls: 'fa fa-file-excel',
+          reference: 'exportButton',
+          text: 'Export',
+          margin: '0 5 0 5',
+          tooltip: 'Export to Excel',
+          disabled: true,
+        },
+        {
+          xtype: 'button',
+          iconCls: 'x-fa fa-question',
+          margin: '0 5 0 5',
+          tooltip: 'View Help',
+        },
+      ],
+      xtype: 'toolbar',
+    },
+  ],
 });
